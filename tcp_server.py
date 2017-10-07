b381d376639f1d6cb3314d681713440d868f6c80
Edmundo Ramírez <edmundormz@gmail.com>
2017-10-07 10:38:54 -0500

    First functional code

--- tcp_server.py
+++ tcp_server.py
@@ -5,7 +5,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = ('172.16.15.183', 8888)
-print >>sys.stderr, ´'starting up on % port %' % server_address
+print >>sys.stderr, 'starting up on port ', server_address
 sock.bind(server_address)
 
 #Listen for incoming connections
@@ -22,7 +22,7 @@ while True:
         # Receive the data in small chnks and retransmit it
         while True:
             data = connection.recv(16) # 32?
-            print >>sys.stderr, 'received "%"' % data
+            print >>sys.stderr, 'received ', data
             if data:
                 print >>sys.stderr, 'sendig data back to the client'
                 connection.sendall(data)
