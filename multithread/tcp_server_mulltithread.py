3ee72063c3945e1029cffec0a03019947740606d
edmundormz <edmundormz@gmail.com>
2017-10-11 00:02:53 -0500

    Adding multithread server code

--- multithread/tcp_server_mulltithread.py
+++ multithread/tcp_server_mulltithread.py
@@ -3,43 +3,34 @@ import sys
 import thread
 import time
 
+def connection_function(connection, client_address):
+    print >>sys.stderr, 'connection from', client_address
+    while True:
+            data = connection.recv(16)
+            print >>sys.stderr, 'received ', data
+            if data:
+                print >>sys.stderr, 'sendig data back to the client'
+                connection.sendall(data)
+            else:
+                print >>sys.stderr, 'no more data from', client_address
+                break        
+    connection.close()
 
 # Create the TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server_address = ('192.168.100.5', 8888)
 print >>sys.stderr, 'starting up on port ', server_address
 sock.bind(server_address)
-
 #Listen for incoming connections
 sock.listen(5)
 
 while True:
     # Wait for a connection
-    print >>sys.stderr, 'Waiting for a connection'
+    print >>sys.stderr, 'waiting for a connection'
     connection, client_address = sock.accept()
-
     try:
-        print >>sys.stderr, 'connection from', client_address
-
-def connection_thread(connection,client_address):
-    
-# # Define a function for the thread
-# def print_time( threadName, delay):
-#    count = 0
-#    while count < 5:
-#       time.sleep(delay)
-#       count += 1
-#       print "%s: %s" % ( threadName, time.ctime(time.time()) )
-
-
-# # Create two threads as follows
-# try:
-#     print "%s" % (time.ctime(time.time()) )
-#     thread.start_new_thread( print_time, ("Thread-1", 2, ) )
-#     thread.start_new_thread( print_time, ("Thread-2", 4, ) )
-# except:
-#    print "Error: unable to start thread"
-
-# while 1:
-#    pass
+        thread.start_new_thread(connection_function,(connection, client_address))
+    except:
+        print "Error: unable to start new thread"
+    finally:
+        print "Server closed"
